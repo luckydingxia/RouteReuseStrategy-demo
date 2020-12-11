585efd19d0e17714a7a0ed09e18db945ca71aaa7
lucylucy-xia <879489713@qq.com>
2020-12-11 16:18:55 +0800

    详情页面美化

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# Sum
+# RouteReuseStrategy-demo
+
+路由复用策略demo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.1.3.
 
-## Development server
+## Example
+
+[在线live](https://github.com/angular/angular-cli)
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/route-reuse-strategy/route-reuse-strategy.module.ts
+++ src/app/route-reuse-strategy/route-reuse-strategy.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { RouteReuseStrategyRoutingModule } from './route-reuse-strategy-routing.module';
 import { MainContainerComponent } from './containers/main-container/main-container.component';
@@ -8,6 +9,7 @@ import { NzTabsModule } from 'ng-zorro-antd/tabs';
 import { DetailComponent } from './containers/detail/detail.component';
 import { MainContainerService } from './services/main-container.service';
 import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzInputModule } from 'ng-zorro-antd/input';
 
 @NgModule({
   declarations: [
@@ -17,9 +19,12 @@ import { NzMessageModule } from 'ng-zorro-antd/message';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouteReuseStrategyRoutingModule,
     NzTabsModule,
-    NzMessageModule
+    NzMessageModule,
+    NzInputModule
   ],
   providers: [
     MainContainerService
